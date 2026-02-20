a53cfb59dbece066d7c78395eece4aed3c869658
Mohd Saquib <saquibansarit09@gmail..com>
2026-02-21 01:19:57 +0530

    Changes Made

--- src/Config.ts
+++ src/Config.ts
@@ -1,2 +1,1 @@
-// export const BACKEND_URL = "https://brainly-back-end-4.onrender.com"
-export const BACKEND_URL = "http://localhost:3000"
+export const BACKEND_URL = "https://brainly-back-end-5.onrender.com"
